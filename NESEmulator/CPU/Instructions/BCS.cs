3d93fb425ee896233cbebf1746a5e018ec01b776
Josh.Siegl <Josh.Siegl@ebs.local>
2019-03-27 14:50:27 -0400

    updates

---
+++ NESEmulator/CPU/Instructions/BCS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NESEmulator.CPU.Instructions
+{
+    /// <summary>
+    /// If the carry flag is set then add the relative displacement to the 
+    /// program counter to cause a branch to a new location.
+    /// </summary>
+    public class BCS : Instruction
+    {
+        public BCS(string OpCode) : 
+            base (OpCode)
+        {
+            switch (OpCode)
+            {
+                case "B0":
+                    this.addressingmode = AddressingMode.Relative;
+                    this.cycles = 2; // +1 if branch succeeds +2 if to a new page
+                    bytes = new byte[2]; 
+                    break; 
+            }
+        }
+    }
+}
---
+++ NESEmulator/CPU/Instructions/BEQ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NESEmulator.CPU.Instructions
+{
+    /// <summary>
+    /// If the zero flag is set then add the relative displacement to the 
+    /// program counter to cause a branch to a new location.
+    /// </summary>
+    public class BEQ : Instruction
+    {
+        public BEQ(string OpCode) 
+            : base(OpCode)
+        {
+            switch (OpCode)
+            {
+                case "F0":
+                    this.addressingmode = AddressingMode.Relative;
+                    this.cycles = 2; //+1 if branch succeeds +2 if to a new page
+                    bytes = new byte[2]; 
+                    break; 
+            }
+        }
+    }
+}
