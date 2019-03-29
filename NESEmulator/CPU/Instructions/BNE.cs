fc23abc0f89d457a9e5fd288addf5854ac85ffa6
Josh.Siegl <Josh.Siegl@ebs.local>
2019-03-29 13:58:10 -0400

    all

--- NESEmulator/CPU/Instructions/BNE.cs
+++ NESEmulator/CPU/Instructions/BNE.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace NESEmulator.CPU.Instructions
 {
-    class BNE
+    /// <summary>
+    /// If the zero flag is clear then add the relative displacement 
+    /// to the program counter to cause a branch to a new location.
+    /// </summary>
+    public class BNE : Instruction
     {
+        public BNE(string OpCode)
+            : base (OpCode)
+        {
+            switch (OpCode)
+            {
+                case "D0":
+                    this.addressingmode = AddressingMode.Relative;
+                    this.cycles = 2; //+1 if branch succeeds +2 if to a new page
+                    bytes = new byte[2]; 
+                    break; 
+            }
+        }
     }
 }
